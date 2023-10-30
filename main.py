39022bb43a62ea6b1c6b59db3c8b5b27d498eee0
Bluemethyst <88925092+Bluemethyst@users.noreply.github.com>
2023-10-31 10:07:25 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-import requests, nextcord
 from nextcord.ext import commands
 from bot_commands import botCommands
 
 bot = commands.Bot()
 bot.add_cog(botCommands(bot))
-
-
-headers = {
-    'application': 'json',
-    'query': 'gravestone'
-}
-r = requests.get('https://api.modrinth.com/v2/search', headers = headers)
-modrinthData = r.json()
 
 @bot.event
 async def on_ready():

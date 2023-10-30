b570f527d9cfb5e8eafd60828e10d8235802a1f4
Bluemethyst <88925092+Bluemethyst@users.noreply.github.com>
2023-10-30 16:01:58 +1300

    Update main.py

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ modrinthData = r.json()
 async def on_ready():
     print(f'We have logged in as {bot.user}')
     
-bot.run('MTE2NzYxNTc2MjQxMjg2MzU1OA.G0mR9D.WXLcSntpLTvmdCKkJ4yWN-goSI1XUQr8TJ9czE')
+bot.run('TOKEN-HERE')
